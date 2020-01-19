c959b5cd666fbf93d42066b07e96be807787aa9d
ultimabear <thebear@yandex.ru>
2020-01-19 19:43:58 +0300

    updated vrchat and udon sdk external libraries and added sample program source asset

---
+++ ExternalDlls/readme.md
@@ -0,0 +1,30 @@
+# external .dll files needed to collect udon graph api data outside of unity3d
+
+## files from unity3d installation
+%UnityInstallation% is C:\Program Files\Unity\Hub\Editor\2018.4.14f1\ in my case
+
+*  %UnityInstallation%\Editor\Data\Managed\UnityEngine.dll
+*  %UnityInstallation%\Editor\Data\UnityExtensions\Unity\GUISystem\UnityEngine.UI.dll
+
+## files from VRCSDK3 and Udon SDK
+%UnityProject% in my case is just path to some unity3d project with imported SDK's in it
+
+* %UnityProject%\Assets\Udon\External\
+    * VRC.Udon.ClientBindings.dll
+    * VRC.Udon.Common.dll
+    * VRC.Udon.Security.dll
+    * VRC.Udon.VM.dll
+    * VRC.Udon.Wrapper.dll
+
+* %UnityProject%\Assets\Udon\Editor\External\
+    * VRC.Udon.Compiler.dll
+    * VRC.Udon.EditorBindings.dll
+    * VRC.Udon.Graph.dll
+    * VRC.Udon.UAssembly.dll
+
+* %UnityProject%\Assets\VRCSDK\Plugins\
+    * VRCCore-Editor.dll
+    * VRCCore-Standalone.dll
+    * VRCSDK3.dll
+    * VRCSDK3-Editor.dll
+    * VRCSDKBase.dll
---
+++ Unity3dProject/Assets/Test/Editor/UdonTestLangProgramAsset.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using VRC.Udon.Common.Interfaces;
+
+[assembly: UdonProgramSourceNewMenu(typeof(UdonTestLangProgramAsset), "Udon Test lang Program Asset")]
+
+[CreateAssetMenu(menuName = "VRChat/Udon/Udon Test lang Program Asset", fileName = "New Udon Test lang Program Asset")]
+public class UdonTestLangProgramAsset : UdonAssemblyProgramAsset
+{
+    protected override void DoRefreshProgramActions()
+    {
+        var values = new Dictionary<string, (object value, Type type)>()
+        {
+            {
+                "axis_0",
+                ( value: new Vector3(0, 0, 1), type: typeof(Vector3) )
+            },
+
+            {
+                "angle_0",
+                ( value: (Single)1, type: typeof(Single) )
+            },
+        };
+
+        udonAssembly = @"
+.data_start
+
+    
+    instance_0: %UnityEngineTransform, this
+    axis_0: %UnityEngineVector3, null
+    angle_0: %SystemSingle, null
+
+.data_end
+
+.code_start
+
+    .export _update
+    
+    _update:
+    
+        PUSH, instance_0
+        PUSH, axis_0
+        PUSH, angle_0
+        EXTERN, ""UnityEngineTransform.__Rotate__UnityEngineVector3_SystemSingle__SystemVoid""
+        JUMP, 0xFFFFFF
+
+.code_end
+        ";
+
+        base.DoRefreshProgramActions();
+
+        ApplyDefaultValuesToHeap(values);
+    }
+
+    protected void ApplyDefaultValuesToHeap(Dictionary<string, (object value, Type type)> heapDefaultValues)
+    {
+        IUdonSymbolTable symbolTable = program?.SymbolTable;
+        IUdonHeap heap = program?.Heap;
+        if (symbolTable == null || heap == null)
+        {
+            return;
+        }
+
+        foreach (KeyValuePair<string, (object value, Type type)> defaultValue in heapDefaultValues)
+        {
+            if (!symbolTable.HasAddressForSymbol(defaultValue.Key))
+            {
+                continue;
+            }
+
+            uint symbolAddress = symbolTable.GetAddressFromSymbol(defaultValue.Key);
+            (object value, Type declaredType) = defaultValue.Value;
+            if (typeof(UnityEngine.Object).IsAssignableFrom(declaredType))
+            {
+                if (value != null && !declaredType.IsInstanceOfType(value))
+                {
+                    heap.SetHeapVariable(symbolAddress, null, declaredType);
+                    continue;
+                }
+                if ((UnityEngine.Object)value == null)
+                {
+                    heap.SetHeapVariable(symbolAddress, null, declaredType);
+                    continue;
+                }
+            }
+
+            if (value != null)
+            {
+                if (!declaredType.IsInstanceOfType(value))
+                {
+                    value = declaredType.IsValueType ? Activator.CreateInstance(declaredType) : null;
+                }
+            }
+
+            heap.SetHeapVariable(symbolAddress, value, declaredType);
+        }
+    }
+}
